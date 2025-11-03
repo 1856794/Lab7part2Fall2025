156a36469aebcbf0fbfeccb9b513ddeb70f0cdb9
Robert Padilla <rdiegop06@gmail.com>
2025-11-02 19:38:52 -0800

    Initial sharing of project

---
+++ ArrayMethods.java
@@ -0,0 +1,111 @@
+import java.util.Arrays;
+/**
+ * creates an int[] array called myArray, with preset values
+ * count() returns the number of values in the array
+ *      through a for each loop
+ * sum() returns the sum of the values in the array
+ *      for loop
+ * average() returns the average of the values in the array
+ *      uses count() and sum()
+ * findMax() is the largest value in myArray
+ *      assigns a max int that goes through each value of myArray
+ * findIndexOfMax() returns the largest index of the max
+ *      uses loops that checks each value if equal to max then returns
+ *      that index
+ *
+ * @author Robert Padilla
+ * @version v1.0
+ * @since 11/2/25
+ */
+public class ArrayMethods
+{
+    // instance variables - replace the example below with your own
+    private int[] myArray = {7, 8, 8, 3, 4, 9, 8, 7};
+
+    
+
+    /**
+     * returns the number of values in myArray
+     *
+     * @return integer counter that counts values in array
+     */
+    public int count()
+    {
+        // put your code here
+        int counter = 0;
+        for(int array: myArray){
+            counter++;
+        }
+        return counter;
+    }
+    
+    /**
+     * returns the sum of the values in myArray
+     * 
+     * @return int values for the sum of the values in the array
+     */
+    public int sum(){
+        int sum = 0;
+        for(int i = 0; i < myArray.length; i++){
+            sum += myArray[i];
+        }
+        return sum;
+    }
+    
+    /**
+     * returns the average of the values in myArray using
+     * sum and count methods
+     *
+     * @return double of the average values in the array
+     */
+    public double average (){
+        return (double)sum()/count();
+    }
+    
+    /**
+     * returns the max value in myArray
+     * 
+     * @return max int value in myArray
+     */
+    public int findMax(){
+        int max = myArray[0];
+        for(int i = 0; i < myArray.length; i++){
+            if(myArray[i] > max)
+                max = myArray[i];
+        }
+        return max;
+    }
+    
+    /**
+     * returns the index of the max value in the array
+     * 
+     * @return index integer of the max value
+     */
+    public int findIndexOfMax(){
+        int index = 0;
+        for(int i = 0; i < myArray.length; i++){
+            
+            if(myArray[i] == findMax()){
+                index = i;
+            }
+        }
+        return index;
+    }
+    
+    /**
+     * prints an int array, nicely formatted
+     * @param inArray int array to print
+     * 
+     */
+    public void print(int[] inArray){
+        System.out.print("{");
+            int i;
+            for(i = 0; i < inArray.length - 1; i++){
+                System.out.print(inArray[i] + ",");
+            }
+            if(inArray.length > 0){
+                System.out.print(inArray[i]);
+            }
+            System.out.println("}");
+    }
+}
